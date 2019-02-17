53df2bb12ae79da71bd9b73233630108b651018f
John Li <john166@rocketmail.com>
2019-02-17 11:39:35 -0800

    why am I still working on this

---
+++ tiredAndDefeated.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define Im          int
+#define so          main
+#define tired       (
+#define _           void
+#define My          )
+#define idea        {
+#define fell        printf
+#define through     (
+#define __          "%s\n"
+#define I           ,
+#define troubleshot "Hello World!"
+#define it          )
+#define forr        ;
+#define about       char* hap = ":)";
+#define six         char* angrycausenoeyes = ">^)";
+#define hours       char* hap2 = ":)";
+#define ___         char* youpokedhiseyeout = ">:(";
+#define the         char* biganger = ">:0";
+#define pizza       char* uniborw = "<:o";
+#define isnt        char* anger2electricboogaloo = ">:(";
+#define sitting     char* hapTheRequiem = ":)))))";
+#define well        char * doublechin= ">:oo";
+#define ____        char* doo = "poo";
+#define II          char* doodoo = "poopoo";
+#define have        char* doodoodoo = ";)";
+#define to          ;
+#define pee         char* please = " H A H A";
+#define _____       char* help = "H A H A H A";
+#define I_am        char* w = ":)";
+#define but         return
+#define tiredd      0
+#define and         ;
+#define defeated    }
